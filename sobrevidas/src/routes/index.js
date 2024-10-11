8969fa53851d0b91c7555b41d45ac58409ed3943
pedroaugusto99 <>
2024-10-11 15:18:52 -0300

    Criação dos arquivos e algumas configs

---
+++ sobrevidas/src/routes/index.js
@@ -0,0 +1,17 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SignIn from '../pages/SignIn'
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen 
+            name="SignIn" 
+            component={SignIn}
+            options={{ headerShown: false }}
+            />
+        </Stack.Navigator>
+    )
+}
